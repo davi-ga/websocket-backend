3f898600bd765b76f8f7d22b034c786cca083158
davi-ga <contato@daviga.dev.br>
2026-06-10 12:01:36 -0300

    feat: add API client fixture and enhance user creation in tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,12 @@
 import pytest
 from features.users.models import User
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
 
 @pytest.fixture
 def user_payload():
@@ -8,4 +15,11 @@ def user_payload():
 
 @pytest.fixture
 def user(db, user_payload):
-    return User.objects.create_user(**user_payload)
+    return User.objects.create_user(**user_payload, is_active=True)
+
+
+@pytest.fixture
+def authenticated_client(user):
+    client = APIClient()
+    client.force_authenticate(user=user)
+    return client
