59079c470dc66417e578960f0de47f351fb82db4
davi-ga <contato@daviga.dev.br>
2026-06-11 17:37:58 -0300

    feat: add health check endpoint to urlpatterns

--- core/api/urls.py
+++ core/api/urls.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path("docs/", include("core.swagger.api.urls")),
+    path("health/", include("core.health.api.urls")),
     path("auth/", include("features.authentication.api.urls")),
     path("users/", include("features.users.api.urls")),
     path("rooms/", include("features.rooms.api.urls")),
