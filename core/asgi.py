dc5533d6f655b92ab238a056a29061f33af52dac
davi-ga <contato@daviga.dev.br>
2026-06-11 11:56:09 -0300

    fix: use routing term

--- core/asgi.py
+++ core/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-from features.rooms.api.urls import ws_urlpatterns
+from features.rooms.routing import ws_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
